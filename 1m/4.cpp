b8928efbf48d9a5b889b42c518f2248efc9ef737
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-03-08 00:38:29 +0300

    4. first try

--- 1m/4.cpp
+++ 1m/4.cpp
@@ -2,7 +2,8 @@
  * Во всех задачах из следующего списка следует написать структуру данных, обрабатывающую команды push* и pop*.
 Формат входных данных.
 В первой строке количество команд n. n ≤ 1000000.
-Каждая команда задаётся как 2 целых числа: a b. a = 1 - push front
+Каждая команда задаётся как 2 целых числа: a b.
+a = 1 - push front
 a = 2 - pop front
 a = 3 - push back
 a = 4 - pop back
@@ -17,6 +18,135 @@ a = 4 - pop back
  * 4_1.  Реализовать очередь с динамическим зацикленным буфером.
  */
 
+#include <iostream>
+#include <cassert>
+#include <memory.h>
+
+using namespace std;
+
+
+class Queue {
+public:
+    Queue(size_t initialCapacity = 32);
+    Queue(const Queue& other);
+    ~Queue();
+
+    void pushBack(int item);
+    int popFront();
+
+private:
+    void _grow();
+    size_t _getPrevIndex(size_t current);
+    size_t _getNextIndex(size_t current);
+
+    int* _data = nullptr;
+    size_t _capacity = 0;
+    size_t _headIndex = 0;
+    size_t _tailIndex = 0;
+};
+
+Queue::Queue(size_t initialCapacity) :
+    _capacity(initialCapacity),
+    _data((int*) malloc(sizeof(int) * initialCapacity)),
+    _headIndex(0),
+    _tailIndex(0)
+{
+}
+
+Queue::Queue(const Queue& other) {
+    _capacity = other._capacity;
+    _headIndex = other._headIndex;
+    _tailIndex = other._tailIndex;
+    if (_data != nullptr) {
+        free(_data);
+    }
+    _data = (int*) malloc(sizeof(int) * _capacity);
+    memcpy(_data, other._data, sizeof(int) * _capacity);
+}
+
+Queue::~Queue() {
+    free(_data);
+}
+
+void Queue::pushBack(int item) {
+    size_t nextTailIndex = _getNextIndex(_tailIndex);
+    if (nextTailIndex == _headIndex) {
+        _grow();
+        nextTailIndex = _getNextIndex(_tailIndex);
+    }
+    _data[_tailIndex] = item;
+    _tailIndex = nextTailIndex;
+}
+
+int Queue::popFront() {
+    if (_headIndex == _tailIndex) {
+        return -1;
+    }
+    int item = _data[_headIndex];
+    _headIndex = _getPrevIndex(_headIndex);
+    return item;
+}
+
+void Queue::_grow() {
+    const size_t kGrowStep = 32;
+    _capacity += kGrowStep;
+    _data = (int*) realloc(_data, sizeof(int) * _capacity);
+    if (_tailIndex < _headIndex) {
+        memcpy(_data + _headIndex + kGrowStep, _data + _headIndex, sizeof(int) * kGrowStep);
+        _headIndex += kGrowStep;
+    }
+}
+
+size_t Queue::_getPrevIndex(size_t current) {
+    if (current != 0) {
+        return current - 1;
+    } else {
+        return _capacity - 1;
+    }
+}
+
+size_t Queue::_getNextIndex(size_t current) {
+    if (current + 1 != _capacity) {
+        return current + 1;
+    } else {
+        return 0;
+    }
+}
+
+
+bool processCommand(Queue& queue, int mode, int value) {
+    switch (mode) {
+        case 2:
+            return queue.popFront() == value;
+
+        case 3:
+            queue.pushBack(value);
+            return true;
+
+        default:
+            return false;
+    }
+}
+
+
 int main() {
+    Queue queue;
+
+    size_t n;
+    cin >> n;
+
+    bool answer = true;
+    for (size_t i = 0; i < n; i++) {
+        int mode, value;
+        cin >> mode;
+        cin >> value;
+
+        if (!processCommand(queue, mode, value)) {
+            answer = false;
+            break;
+        }
+    }
+    cout << (answer ? "YES" : "NO");
+
     return 0;
 }
