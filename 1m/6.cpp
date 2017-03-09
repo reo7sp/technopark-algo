73c15aa7e10f64e8037ee129d97ef79387f0a743
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-03-09 12:40:34 +0300

    6. first try

--- 1m/6.cpp
+++ 1m/6.cpp
@@ -10,6 +10,35 @@
  * 6_2. Широкая пирамида.  Каждый вышележащий слой пирамиды должен быть строго меньше нижележащего.
  */
 
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+
+int countPossiblePiramyds(int n, int minCubesOnLevel = 0) {
+    assert(n >= 1);
+    assert(minCubesOnLevel >= 0);
+    if (n < minCubesOnLevel) {
+        return 0;
+    }
+    if (n == minCubesOnLevel) {
+        return 1;
+    }
+    int result = 0;
+    for (int i = minCubesOnLevel + 1; i <= n; i++) {
+        result += countPossiblePiramyds(n - minCubesOnLevel, i);
+    }
+    return result;
+}
+
+
 int main() {
+    int n;
+    cin >> n;
+
+    cout << countPossiblePiramyds(n);
+
     return 0;
 }
+
