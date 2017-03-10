0dfadc8d3c3c6f68292d6030aa46905e0fca291d
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-03-11 00:27:38 +0300

    7. first try

--- 1m/7.cpp
+++ 1m/7.cpp
@@ -8,6 +8,102 @@
 Выход должен содержать натуральное число  — максимальное число заявок.
  */
 
+#include <iostream>
+#include <cassert>
+#include <memory.h>
+
+using namespace std;
+
+
+class IntMap {
+public:
+    explicit IntMap(size_t initialCapacity = 32);
+    IntMap(const IntMap& other);
+    ~IntMap();
+
+    int& operator[](size_t index);
+
+    size_t getCapacity() const;
+
+private:
+    void _grow(size_t minNewCapacity);
+
+    size_t _capacity = 0;
+    int* _data = nullptr;
+};
+
+IntMap::IntMap(size_t initialCapacity) :
+        _capacity(initialCapacity),
+        _data((int*) calloc(_capacity, sizeof(int)))
+{
+}
+
+IntMap::IntMap(const IntMap &other) {
+    _capacity = other._capacity;
+    if (_data != nullptr) {
+        free(_data);
+    }
+    _data = (int*) calloc(_capacity, sizeof(int));
+    memcpy(_data, other._data, sizeof(int) * _capacity);
+}
+
+IntMap::~IntMap() {
+    free(_data);
+}
+
+int& IntMap::operator[](size_t index) {
+    if (index >= _capacity) {
+        _grow(index);
+    }
+    return _data[index];
+}
+
+size_t IntMap::getCapacity() const {
+    return _capacity;
+}
+
+void IntMap::_grow(size_t minNewCapacity) {
+    const size_t kGrowStep = 32;
+    size_t oldCapacity = _capacity;
+    _capacity = minNewCapacity + kGrowStep;
+    _data = (int*) realloc(_data, sizeof(int) * _capacity);
+    memset(_data + oldCapacity, 0, _capacity - oldCapacity);
+}
+
+
 int main() {
+    IntMap map;
+
+    int minTime = -1, maxTime = -1;
+    
+    int startTime, endTime;
+    while (cin >> startTime && cin >> endTime) {
+        int duration = endTime - startTime + 1;
+        assert(duration > 0);
+        map[duration]++;
+        if (minTime == -1 || startTime < minTime) {
+            minTime = startTime;
+        }
+        if (maxTime == -1 || endTime > maxTime) {
+            maxTime = endTime;
+        }
+    }
+
+    int leftTime = maxTime - minTime + 1;
+    int answer = 0;
+    bool stop = false;
+    for (size_t i = 0; !stop && i < map.getCapacity(); i++) {
+        for (size_t j = 0; j < map[i]; j++) {
+            leftTime -= i;
+            if (leftTime >= 0) {
+                answer++;
+            } else {
+                stop = true;
+                break;
+            }
+        }
+    }
+    cout << answer;
+
     return 0;
 }
