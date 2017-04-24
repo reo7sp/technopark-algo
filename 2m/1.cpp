dc82e6c9dcf757f61c58b236ca8d87e8c4241344
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-04-25 00:36:51 +0300

    1. done

--- 2m/1.cpp
+++ 2m/1.cpp
@@ -24,3 +24,103 @@
      * out:
          0 1
  */
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+class Box {
+public:
+    Box(int sizeX, int sizeY, int sizeZ);
+
+    int getSizeX() const;
+    int getSizeY() const;
+    int getSizeZ() const;
+
+private:
+    int _sizeX, _sizeY, _sizeZ;
+};
+
+Box::Box(int sizeX, int sizeY, int sizeZ) : _sizeX(sizeX), _sizeY(sizeY), _sizeZ(sizeZ) {}
+
+int Box::getSizeX() const {
+    return _sizeX;
+}
+
+int Box::getSizeY() const {
+    return _sizeY;
+}
+
+int Box::getSizeZ() const {
+    return _sizeZ;
+}
+
+
+class BoxWithId : public Box {
+public:
+    BoxWithId(size_t id, int sizeX, int sizeY, int sizeZ);
+
+    size_t getId() const;
+
+private:
+    size_t _id;
+};
+
+BoxWithId::BoxWithId(size_t id, int sizeX, int sizeY, int sizeZ) : _id(id), Box(sizeX, sizeY, sizeZ) {}
+
+size_t BoxWithId::getId() const {
+    return _id;
+}
+
+
+template<typename T>
+class BoxIsLessComparer {
+public:
+    bool operator()(const T& left, const T& right) const;
+};
+
+template<typename T>
+bool BoxIsLessComparer<T>::operator()(const T& left, const T& right) const {
+    return left.getSizeX() < right.getSizeX() && left.getSizeY() < right.getSizeY() && left.getSizeZ() < right.getSizeZ();
+};
+
+
+template<typename T, typename COMPARER>
+void insertSort(vector<T>& array, const COMPARER& compare) {
+    for (size_t i = 1; i < array.size(); i++)  {
+        T tmp = array[i];
+        size_t j = i;
+        for (; j >= 1 && compare(tmp, array[j - 1]); j--) {
+            array[j] = array[j - 1];
+        }
+        array[j] = tmp;
+    }
+};
+
+
+int main() {
+    vector<BoxWithId> boxes;
+    BoxIsLessComparer<BoxWithId> boxCompare;
+
+    size_t n;
+    cin >> n;
+    boxes.reserve(n);
+
+    for (size_t i = 0; i < n; i++) {
+        int x, y, z;
+        cin >> x;
+        cin >> y;
+        cin >> z;
+        boxes.push_back(BoxWithId(i, x, y, z));
+    }
+
+    insertSort(boxes, boxCompare);
+
+    for (size_t i = 0; i < n; i++) {
+        cout << boxes[i].getId() << ' ';
+    }
+
+    return 0;
+}
