1f6027f999f9932cb5085fec417c02fb800ca902
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-04-27 04:27:57 +0300

    5. wip

--- 2m/5.cpp
+++ 2m/5.cpp
@@ -11,3 +11,40 @@
      * out:
          0 1 2 3 4 5 6 7 8 9
  */
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+
+vector<int> sortSemisortedSequence(const vector<int>& input, size_t k) {
+//    mergeSort()
+    return vector<int>();
+}
+
+
+int main() {
+    size_t n;
+    cin >> n;
+
+    size_t k;
+    cin >> k;
+
+    vector<int> input;
+    input.reserve(n);
+
+    for (size_t i = 0; i < n; i++) {
+        int it;
+        cin >> it;
+        input.push_back(it);
+    }
+
+    vector<int> output = sortSemisortedSequence(input, k);
+
+    for (size_t i = 0; i < output.size(); i++) {
+        cout << output[i] << " ";
+    }
+
+    return 0;
+}
