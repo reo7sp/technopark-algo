27349c7937bb1bf4d210e268ce86bc55850b3ccd
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-04-28 07:09:18 +0300

    4. done

--- 2m/4.cpp
+++ 2m/4.cpp
@@ -36,3 +36,99 @@
      * out:
          1
  */
+
+#include <cstddef>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+namespace findOrderStatisticUtils {
+    /**
+     * Медиана из первого, последнего и серединного.
+     * @param startIndex inclusive
+     * @param endIndex exclusive
+     */
+    template<typename T>
+    inline size_t getPivot(const vector<T>& array, size_t startIndex, size_t endIndex) {
+        endIndex--;
+        size_t middleIndex = (startIndex + endIndex) / 2;
+        // перебираем по кругу
+        if (array[endIndex] <= array[startIndex] && array[startIndex] <= array[middleIndex]) {
+            return startIndex;
+        } else if (array[startIndex] <= array[middleIndex] && array[middleIndex] <= array[endIndex]) {
+            return middleIndex;
+        } else {
+            return endIndex;
+        }
+    }
+
+
+    /**
+     * @param startIndex inclusive
+     * @param endIndex exclusive
+     */
+    template<typename T>
+    size_t partition(vector<T>& array, size_t startIndex, size_t endIndex) {
+        size_t pivotIndex = getPivot(array, startIndex, endIndex);
+
+        endIndex--; // right is inclusive now
+
+        T pivot = array[pivotIndex];
+        swap(array[pivotIndex], array[endIndex]); // pivot must not interfere in comparisons
+
+        size_t i = startIndex;
+        size_t j = startIndex;
+        while (j < endIndex) {
+            if (array[j] <= pivot) {
+                swap(array[i], array[j]);
+                i++;
+            }
+            j++;
+        }
+
+        swap(array[i], array[endIndex]); // revert pivot
+
+        return i;
+    }
+};
+
+
+template<typename T>
+T findOrderStatistic(vector<T>& array, size_t k) {
+    size_t pivotIndex;
+    size_t startIndex = 0;
+    size_t endIndex = array.size();
+    while (startIndex < endIndex) {
+        pivotIndex = findOrderStatisticUtils::partition(array, startIndex, endIndex);
+        if (pivotIndex == k) {
+            break;
+        } else if (pivotIndex < k) {
+            startIndex = pivotIndex + 1;
+        } else {
+            endIndex = pivotIndex;
+        }
+    }
+    return array[pivotIndex];
+}
+
+
+int main() {
+    size_t n;
+    cin >> n;
+
+    size_t k;
+    cin >> k;
+
+    vector<int> input;
+    input.reserve(n);
+    for (size_t i = 0; i < n; i++) {
+        int it;
+        cin >> it;
+        input.push_back(it);
+    }
+
+    cout << findOrderStatistic(input, k);
+
+    return 0;
+}
