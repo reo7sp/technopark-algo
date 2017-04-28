6797bf964c1f0f8a451e2be0b4bd77cb246e14c1
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-04-28 03:25:55 +0300

    5. passed

--- 2m/5.cpp
+++ 2m/5.cpp
@@ -18,9 +18,63 @@
 using namespace std;
 
 
-vector<int> sortSemisortedSequence(const vector<int>& input, size_t k) {
-//    mergeSort()
-    return vector<int>();
+template<typename T>
+vector<T> merge(typename vector<T>::const_iterator begin1, typename vector<T>::const_iterator end1, typename vector<T>::const_iterator begin2, typename vector<T>::const_iterator end2) {
+    vector<T> result;
+    result.reserve((end1 - begin1) + (end2 - begin2));
+    while (begin1 != end1 && begin2 != end2) {
+        if (*begin1 <= *begin2) {
+            result.push_back(*begin1);
+            begin1++;
+        } else {
+            result.push_back(*begin2);
+            begin2++;
+        }
+    }
+    while (begin1 != end1) {
+        result.push_back(*begin1);
+        begin1++;
+    }
+    while (begin2 != end2) {
+        result.push_back(*begin2);
+        begin2++;
+    }
+    return result;
+}
+
+template<typename T>
+vector<T> mergeSort(typename vector<T>::const_iterator begin, typename vector<T>::const_iterator end) {
+    if (end - begin == 1) {
+        return vector<T> { *begin };
+    }
+    typename vector<T>::const_iterator middle = begin + (end - begin) / 2;
+    vector<T> v1 = mergeSort<T>(begin, middle);
+    vector<T> v2 = mergeSort<T>(middle, end);
+    return merge<T>(v1.cbegin(), v1.cend(), v2.cbegin(), v2.cend());
+}
+
+template<typename T>
+vector<T> mergeSort(const vector<T>& array) {
+    return mergeSort<T>(array.cbegin(), array.cend());
+}
+
+template<typename T>
+vector<T> sortSemisortedSequence(vector<T> array, size_t k) {
+    for (size_t i = 0; i < array.size(); i += k) {
+        typename vector<T>::const_iterator begin = array.cbegin() + i;
+        typename vector<T>::const_iterator end = begin + 2 * k;
+        if (array.cend() < end) {
+            end = array.cend();
+        }
+        typename vector<T>::const_iterator middle = begin + (end - begin) / 2;
+        vector<T> v1 = mergeSort<T>(begin, middle);
+        vector<T> v2 = mergeSort<T>(middle, end);
+        vector<T> sorted = merge<T>(v1.cbegin(), v1.cend(), v2.cbegin(), v2.cend());
+        for (size_t j = 0; j < 2 * k && i + j < array.size(); j++) {
+            array[i + j] = sorted[j];
+        }
+    }
+    return array;
 }
 
 
